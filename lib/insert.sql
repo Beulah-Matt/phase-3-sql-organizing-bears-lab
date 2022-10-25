6e98337c48bd48f375d6ce957c38cfa3a4ea666d
Beulah Mathenge <beulahmathenge@gmail.com>
2022-10-25 16:44:25 +0300

    Complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "Brown", "Calm", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 5, "M", "Grey", "Rowdy", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 7, "F", "White", "Angry", FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Seargent Brown", 17, "M", "Brown", "Cool", FALSE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 10, "F", "Black", "Calm", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 50, "M", "Green", "Grumpy", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 4, "F", "White", "Calm", TRUE);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, "M", NULL, NULL, FALSE);
